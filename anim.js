8ad45adedb87ba118104b526698e145863784dd3
MegaWiler <50437818+MegaWiler@users.noreply.github.com>
2026-03-20 03:46:22 -0500

    Update anim.js

--- anim.js
+++ anim.js
@@ -1,3 +1,6 @@
-onload = () => {
+// anim.js
+window.onload = () => {
+    // Elimina la clase que evita que las flores carguen
     document.body.classList.remove("container");
+    console.log("Animaciones de flores activadas");
 };
