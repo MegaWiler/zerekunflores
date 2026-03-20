245d42f56430b8a6b728c1e7a57b7c59ddbf066a
MegaWiler <50437818+MegaWiler@users.noreply.github.com>
2026-03-20 03:22:35 -0500

    Update anim.js

--- anim.js
+++ anim.js
@@ -1,6 +1,14 @@
 window.onload = () => {
-  const c = setTimeout(() => {
-    document.body.classList.remove("not-loaded");
-    clearTimeout(c);
-  }, 1000);
+    const mensaje = document.getElementById('mensaje-especial');
+    
+    // 1. Después de 6 segundos, quitamos el mensaje
+    setTimeout(() => {
+        mensaje.style.opacity = '0';
+        
+        // 2. Después de que se borre el mensaje, crecen las flores
+        setTimeout(() => {
+            mensaje.style.display = 'none';
+            document.body.classList.remove("container");
+        }, 2000);
+    }, 6000);
 };
