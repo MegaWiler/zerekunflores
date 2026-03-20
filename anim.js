01b037fecd79037ce6384ec690d21e0e3d634de2
MegaWiler <50437818+MegaWiler@users.noreply.github.com>
2026-03-20 04:08:26 -0500

    Update anim.js

--- anim.js
+++ anim.js
@@ -1,6 +1,5 @@
 window.onload = () => {
-    // Esperamos 1 segundo y activamos las flores
     setTimeout(() => {
         document.body.classList.remove("not-loaded");
-    }, 1000);
+    }, 500);
 };
