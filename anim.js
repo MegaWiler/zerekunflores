a3639f624ef1773efb0e4d3028e1a9d4266a5575
MegaWiler <50437818+MegaWiler@users.noreply.github.com>
2026-03-20 04:36:02 -0500

    Update anim.js

--- anim.js
+++ anim.js
@@ -1,3 +1,6 @@
 window.onload = () => {
-    document.body.classList.remove("not-loaded");
+    const c = setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+      clearTimeout(c);
+    }, 1000);
 };
