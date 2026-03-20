ffd7a4071c907d580c5a51a34891cfb560e3234d
MegaWiler <50437818+MegaWiler@users.noreply.github.com>
2026-03-20 04:02:20 -0500

    Update anim.js

--- anim.js
+++ anim.js
@@ -1,9 +1,6 @@
 window.onload = () => {
-    // Esto quita el bloqueo de carga y activa la animación de las flores
-    const timer = setTimeout(() => {
+    // Esperamos 1 segundo y activamos las flores
+    setTimeout(() => {
         document.body.classList.remove("not-loaded");
-        // Si tu CSS usa la clase "container" para ocultarlas, esto la quita:
-        document.body.classList.remove("container");
-        clearTimeout(timer);
     }, 1000);
 };
